70f0e9f03912c3fb1e731306fae9ab294026e6b4
Zhendong Tang <zhendongtt@gmail.com>
2021-12-14 12:23:20 -0500

    basic input function

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = () => {
+  const [term, setTerm] = useState("");
+  const clickHandler = () => {
+    console.log(term);
+  };
   return (
     <div>
       <h1>this is child</h1>
+      <input
+        type="text"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      ></input>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
     </div>
   );
 };
