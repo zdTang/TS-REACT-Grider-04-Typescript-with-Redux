1b967c82c8bc5280d0adcc58e69d9f8321d1a175
Zhendong Tang <zhendongtt@gmail.com>
2021-12-14 13:03:35 -0500

    can acheive basic function

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,8 +2,22 @@ import React, { useState } from "react";
 
 const Input = () => {
   const [term, setTerm] = useState("");
-  const clickHandler = () => {
-    console.log(term);
+  const [Repository, setRepository] = useState<any>(null);
+  const [isError, setIsError] = useState(false);
+  const [Error, setError] = useState("");
+  const clickHandler = async () => {
+    // Get data from Api
+    const result = await fetch(
+      `https://registry.npmjs.org/-/v1/search?text=${term}`
+    );
+    if (!result.ok) {
+      setIsError(true);
+      setError("Request wrong!");
+    }
+    // update our state
+    const resultJson = await result.json();
+    setRepository(resultJson.objects);
+    console.log(resultJson.objects);
   };
   return (
     <div>
@@ -16,6 +30,11 @@ const Input = () => {
       <div>
         <button onClick={clickHandler}>Submit</button>
       </div>
+      {isError && Error}
+      {Repository &&
+        Repository.map((item: any) => (
+          <div key={item.package.name}>{item.package.name}</div>
+        ))}
     </div>
   );
 };
